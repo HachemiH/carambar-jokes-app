33af92d1dbac65a0eeca1d7844578239dddf9f54
Hachemi <hachemi@protonmail.com>
2024-06-06 15:11:32 +0200

    feat: add html + render.yaml

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { Sequelize, Joke } = require("./models");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware pour parser le JSON
+app.use(express.json());
+
+// Route pour récupérer une blague aléatoire
+app.get("/api/jokes/random", async (req, res) => {
+  try {
+    const joke = await Joke.findOne({ order: Sequelize.literal("RANDOM()") });
+    if (joke) {
+      res.json(joke);
+    } else {
+      res.status(404).json({ message: "No jokes found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+const path = require("path");
+
+// Servir le fichier index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Démarrer le serveur
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
